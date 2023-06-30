b87c3298f92ae3ff8db347672e245028d0a3312a
Benjamin Sage <bls@MacBook-Ben.local>
2023-06-29 21:28:36 -0700

    User state

---
+++ WealthLeaderboard/WealthLeaderboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  WealthLeaderboardApp.swift
+//  WealthLeaderboard
+//
+//  Created by Benjamin Sage on 6/29/23.
+//
+
+import SwiftUI
+
+@main
+struct WealthLeaderboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
